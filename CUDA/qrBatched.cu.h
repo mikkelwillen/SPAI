2ad24d0fa36f040c907d18ee6411109eebc1333a
mikkelwillen <mikkel@willen.dk>
2023-04-20 14:55:51 +0200

    new file for qrBatched

--- CUDA/qrBatched.cu.h
+++ CUDA/qrBatched.cu.h
@@ -73,7 +73,7 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
     //     gpuAssert(
     //         cudaMemcpy(h_Tau + i, d_Tau[i], sizeof(float), cudaMemcpyDeviceToHost));
     // }
-
+    printf("after cublasSgeqrfBatched\n");
     printKernel <<< 1, ltau >>> (d_Tau[0], ltau);
     gpuAssert(
         cudaDeviceSynchronize());
