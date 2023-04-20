a39130fb8316842d67c13d08dd32ad39da201f41
mikkelwillen <mikkel@willen.dk>
2023-04-20 16:00:56 +0200

    qrBatched

---
+++ CUDA/simpleKernel.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+__global__ void simpleKernel(int length) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid < length) {
+        printf("Hello World! I'm thread %d", tid);
+    }
+}
+
+int main() {
+    int length = 10;
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (length + threadsPerBlock - 1) / threadsPerBlock;
+    simpleKernel<<<blocksPerGrid, threadsPerBlock>>>(length);
+    cudaDeviceSynchronize();
+    return 0;
+}
