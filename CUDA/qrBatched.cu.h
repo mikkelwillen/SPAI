9f79d3ad8f9c3426af31e3010858ce7fbb10ff1b
mikkelwillen <mikkel@willen.dk>
2023-04-20 13:13:23 +0200

    new file for qrBatched

--- CUDA/qrBatched.cu.h
+++ CUDA/qrBatched.cu.h
@@ -19,7 +19,7 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
         printf("\ncublas error: %d\n", stat);
     }
 
-    int lda = n1;
+    int lda = 0;
     int min = MIN(n1, n2);
     int ltau = MAX(1, min);
     float* d_AHat;
