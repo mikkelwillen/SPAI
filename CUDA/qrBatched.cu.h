b1c189051d95fc2f7cef4415d5ff28bd3c1d4fa3
mikkelwillen <mikkel@willen.dk>
2023-04-24 11:13:37 +0200

    qrBatched

--- CUDA/qrBatched.cu.h
+++ CUDA/qrBatched.cu.h
@@ -75,50 +75,50 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
         h_Tau[i] = tau + i * ltau;
     }
 
-    // print h_AHat
-    printf("\nh_AHat");
-    // printDeviceArrayKernel <<< 1, n1 * n2 >>> (h_AHat, n1 * n2);
-    printf("ltau: %d\n", ltau);
+    // // print h_AHat
+    // printf("\nh_AHat");
+    // // printDeviceArrayKernel <<< 1, n1 * n2 >>> (h_AHat, n1 * n2);
+    // printf("ltau: %d\n", ltau);
 
-    // qr initialization
-    printf("malloc d_AHat\n");
-    gpuAssert(
-        cudaMalloc((void**) &d_Tau, tauMemSize));
-    printf("malloc d_Tau\n");
+    // // qr initialization
+    // printf("malloc d_AHat\n");
+    // gpuAssert(
+    //     cudaMalloc((void**) &d_Tau, tauMemSize));
+    // printf("malloc d_Tau\n");
 
-    gpuAssert(
-        cudaMemcpy(d_AHat, h_AHat, AHatMemSize, cudaMemcpyHostToDevice));
-    printf("copy AHat to d_AHat\n");
-    gpuAssert(
-        cudaMemcpy(d_Tau, h_Tau, tauMemSize, cudaMemcpyHostToDevice));
-    printf("memset d_Tau\n");
+    // gpuAssert(
+    //     cudaMemcpy(d_AHat, h_AHat, AHatMemSize, cudaMemcpyHostToDevice));
+    // printf("copy AHat to d_AHat\n");
+    // gpuAssert(
+    //     cudaMemcpy(d_Tau, h_Tau, tauMemSize, cudaMemcpyHostToDevice));
+    // printf("memset d_Tau\n");
 
-    // stat = cublasSgeqrfBatched(cHandle,
-    //                             n1,
-    //                             n2,
-    //                             d_AHat,
-    //                             lda,
-    //                             d_Tau,
-    //                             &info,
-    //                             BATCHSIZE);
+    // // stat = cublasSgeqrfBatched(cHandle,
+    // //                             n1,
+    // //                             n2,
+    // //                             d_AHat,
+    // //                             lda,
+    // //                             d_Tau,
+    // //                             &info,
+    // //                             BATCHSIZE);
     
-    // if (info != 0) {
-    //     printf("\nparameters are invalid\n");
-    // }
-    // if (stat != CUBLAS_STATUS_SUCCESS) {
-    //     printf("\ncublasSgeqrfBatched failed");
-    //     printf("\ncublas error: %d\n", stat);
-    // }
+    // // if (info != 0) {
+    // //     printf("\nparameters are invalid\n");
+    // // }
+    // // if (stat != CUBLAS_STATUS_SUCCESS) {
+    // //     printf("\ncublasSgeqrfBatched failed");
+    // //     printf("\ncublas error: %d\n", stat);
+    // // }
 
-    // for (int i = 0; i < ltau * ltau; i++) {
-    //     gpuAssert(
-    //         cudaMemcpy(h_Tau + i, d_Tau[i], sizeof(float), cudaMemcpyDeviceToHost));
-    // }
-    printf("after cublasSgeqrfBatched\n");
-    int numberOfBlocks = (n1 * n2 + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    printKernel <<< numberOfBlocks,  BLOCK_SIZE >>> (n1 * n2);
-    gpuAssert(
-        cudaDeviceSynchronize());
+    // // for (int i = 0; i < ltau * ltau; i++) {
+    // //     gpuAssert(
+    // //         cudaMemcpy(h_Tau + i, d_Tau[i], sizeof(float), cudaMemcpyDeviceToHost));
+    // // }
+    // printf("after cublasSgeqrfBatched\n");
+    // int numberOfBlocks = (n1 * n2 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    // printKernel <<< numberOfBlocks,  BLOCK_SIZE >>> (n1 * n2);
+    // gpuAssert(
+    //     cudaDeviceSynchronize());
     // printf("after printKernel\n");
     // gpuAssert(
     //     cudaMemcpy(h_Tau, d_Tau, tauMemSize, cudaMemcpyDeviceToHost));
