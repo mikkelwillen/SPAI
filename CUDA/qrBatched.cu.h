c0bebbd40777badbb13af37945cb0bc2886f16b4
mikkelwillen <mikkel@willen.dk>
2023-04-24 13:19:54 +0200

    qrBatched

--- CUDA/qrBatched.cu.h
+++ CUDA/qrBatched.cu.h
@@ -29,7 +29,7 @@ __global__ void printDeviceArrayKernel(float* h_AHat, int length) {
 __global__ void deviceToDevicePointerKernel(float** d_AHat, float* h_AHat, int batch, int n1, int n2) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid < n1 * n2) {
-        d_AHat[batch][tid] = h_AHat[tid];
+        d_AHat = &h_AHat
     }
 }
 
@@ -70,9 +70,7 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
             cudaMemcpy(h_AHat, AHat, n1 * n2 * sizeof(float), cudaMemcpyHostToDevice));
         gpuAssert(
             cudaMalloc((void**) &d_AHat, BATCHSIZE * sizeof(float*)));
-        // deviceToDevicePointerKernel <<< 1, BATCHSIZE >>> (d_AHat, h_AHat, i, n1, n2);
-        gpuAssert(
-            cudaMemcpy(d_AHat[i], h_AHat, AHatMemSize, cudaMemcpyDeviceToDevice));
+        deviceToDevicePointerKernel <<< 1, BATCHSIZE >>> (d_AHat, h_AHat, i, n1, n2);
         printf("d_AHat: \n");
         printDeviceArrayPointerKernel <<< 1, n1 * n2 >>> (d_AHat, n1 * n2, i);
         h_Tau[i] = tau + i * ltau;
