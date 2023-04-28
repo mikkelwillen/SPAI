108efb2360d46e6dc122677bb85a5e6d6ee2e669
mikkelwillen <mikkel@willen.dk>
2023-04-28 13:28:51 +0200

    qrBatched

--- CUDA/qrBatched.cu.h
+++ CUDA/qrBatched.cu.h
@@ -12,6 +12,12 @@
 // vi skal have kigget på nogle forskellige størrelser
 // fx skal vi finde ud af, om vi vil kopiere alt data til GPU'en med det samme, eller om vi skal kopiere hver aHat separat og så lave en batch af dem
 
+// Kernel to copy d_AHat to d_PointerAHat
+// d_AHat is an array of batch matrices
+// d_PointerAHat is an array of pointers to the start of each matrix in d_AHat
+// batch is the BATCHSIZE
+// n1 is the number of rows in each matrix
+// n2 is the number of columns in each matrix
 __global__ void AHatDeviceToDevicePointerKernel(float** d_AHat, float* h_AHat, int batch, int n1, int n2) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid < BATCHSIZE) {
@@ -19,6 +25,11 @@ __global__ void AHatDeviceToDevicePointerKernel(float** d_AHat, float* h_AHat, i
     }
 }
 
+// Kernel to copy d_tau to d_PointerTau
+// d_tau is an array of batch tau vectors
+// d_PointerTau is an array of pointers to the start of each tau vector in d_tau
+// batch is the BATCHSIZE
+// ltau is the length of each tau vector
 __global__ void tauDeviceToDevicePointerKernel(float** d_Tau, float* h_Tau, int batch, int ltau) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid < BATCHSIZE * ltau) {
@@ -26,17 +37,15 @@ __global__ void tauDeviceToDevicePointerKernel(float** d_Tau, float* h_Tau, int 
     }
 }
 
-__global__ void tauDevicePointerToDeviceKernel(float** d_tau, float* h_tau, int batch, int n1, int n2) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < BATCHSIZE * n1 * n2) {
-        int i = tid / n1;
-        int j = tid % n1;
-        h_tau[tid] = d_tau[i][j];
-    }
-}
-
+// Function to do QR decomposition of batch AHat matrices
+// AHat is an array of batch matrices
+// n1 is the max number of rows of the matrices
+// n2 is the max number of columns of the matrices
+// Q is an array of batch Q matrices
+// R is an array of batch R matrices
 int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
     printf("\nDo QR decomposition of AHat\n");
+
     // create cublas handle
     cublasHandle_t cHandle;
     cublasStatus_t stat = cublasCreate(&cHandle);
@@ -45,14 +54,16 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
         printf("\ncublas error: %d\n", stat);
     }
 
+    // Set constants
     int lda = n1;
     int min = MIN(n1, n2);
     int ltau = MAX(1, min);
-    int n1T = n1;
     const size_t tauMemSize = ltau * BATCHSIZE * sizeof(float);
     const size_t tauPointerMemSize = BATCHSIZE * sizeof(float*);
     const size_t AHatMemSize = n1 * n2 * BATCHSIZE * sizeof(float);
     const size_t AHatPointerMemSize = BATCHSIZE * sizeof(float*);
+
+    // create input and output arrays
     float* h_tau = (float*) malloc(tauMemSize);
     float* d_AHat;
     float* d_tau;
@@ -60,6 +71,7 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
     float** d_PointerTau;
     int info;
 
+    // malloc space and copy data for AHat
     gpuAssert(
         cudaMalloc((void**) &d_AHat, AHatMemSize));
     gpuAssert(
@@ -68,12 +80,15 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
         cudaMalloc((void**) &d_PointerAHat, AHatPointerMemSize));
     AHatDeviceToDevicePointerKernel <<< 1, BATCHSIZE >>> (d_PointerAHat, d_AHat, BATCHSIZE, n1, n2);
     
+    // malloc space for tau
     gpuAssert(
         cudaMalloc((void**) &d_tau, tauMemSize));
     gpuAssert(
         cudaMalloc((void**) &d_PointerTau, tauPointerMemSize));
     tauDeviceToDevicePointerKernel <<< 1, BATCHSIZE * n1T >>> (d_PointerTau, d_tau, BATCHSIZE, n1T);
 
+    // run QR decomposition from cublas
+    // cublas docs: https://docs.nvidia.com/cuda/cublas
     stat = cublasSgeqrfBatched(cHandle,
                                 n1,
                                 n2,
@@ -83,43 +98,22 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
                                 &info,
                                 BATCHSIZE);
     
+    // error handling
     if (info != 0) {
-        printf("\nparameters are invalid\n");
+        printf("Parameter error %d in QR decomposition\n", info);
     }
     if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf("\ncublasSgeqrfBatched failed");
-        printf("\ncublas error: %d\n", stat);
+        printf("cublasSgeqrfBatched failed\n");
+        printf("cublas error: %d\n", stat);
     }
 
-    printf("after cublasSgeqrfBatched\n");
-
+    // copy AHat and tau back to host
     gpuAssert(
         cudaMemcpy(AHat, d_AHat, AHatMemSize, cudaMemcpyDeviceToHost));
     gpuAssert(
         cudaMemcpy(h_tau, d_tau, tauMemSize, cudaMemcpyDeviceToHost));
     
-    // print tau
-    for (int i = 0; i < BATCHSIZE; i++) {
-        printf("tau %d:", i);
-        for (int k = 0; k < ltau; k++) {
-            printf("%f ", h_tau[i * ltau + k]);
-        }
-        printf("\n");
-    }
-    
-    // print AHat
-    for (int i = 0; i < BATCHSIZE; i++) {
-        printf("AHat %d:\n", i);
-        for (int j = 0; j < n1; j++) {
-            for (int k = 0; k < n2; k++) {
-                printf("%f ", AHat[i * n1 * n2 + j * n2 + k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-    
-    // make R
+    // copy R from AHat
     for (int i = 0; i < n2; i++) {
         for (int j = 0; j < n1; j++) {
             if (i >= j) {
@@ -129,7 +123,6 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
             }
         }
     }
-
 
     // make Q with Algorithm 1 from Kerr Campbell Richards QRD on GPUs
     // set Q to I
@@ -176,53 +169,49 @@ int qrBatched(float* AHat, int n1, int n2, float* Q, float* R) {
                 Q[i * n1 + j] -= Qvvt[i * n1 + j];
             }
         }
+    }
 
-        printf("Q_%d:\n", k);
+    // print various matrices
+    {
+        printf("print R\n");
+        for (int j = 0; j < n1; j++) {
+            for (int i = 0; i < n2; i++) {
+                printf("%f ", R[i * n1 + j]);
+            }
+            printf("\n");
+        }
+
+        printf("print Q\n");
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n1; j++) {
                 printf("%f ", Q[i * n1 + j]);
             }
             printf("\n");
         }
-    }
 
-
-    printf("print R\n");
-    for (int j = 0; j < n1; j++) {
-        for (int i = 0; i < n2; i++) {
-            printf("%f ", R[i * n1 + j]);
-        }
-        printf("\n");
-    }
-
-    printf("print Q\n");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n1; j++) {
-            printf("%f ", Q[i * n1 + j]);
-        }
-        printf("\n");
-    }
-
-    // compute Q * R
-    float* QR = (float*) malloc(n1 * n2 * sizeof(float));
-    for (int i = 0; i < n2; i++) {
-        for (int j = 0; j < n1; j++) {
-            QR[i * n1 + j] = 0;
-            for (int k = 0; k < n1; k++) {
-                QR[i * n1 + j] += Q[j * n1 + k] * R[i * n1 + k];
+        // print tau
+        for (int i = 0; i < BATCHSIZE; i++) {
+            printf("tau %d:", i);
+            for (int k = 0; k < ltau; k++) {
+                printf("%f ", h_tau[i * ltau + k]);
             }
+            printf("\n");
+        }
+        
+        // print AHat
+        for (int i = 0; i < BATCHSIZE; i++) {
+            printf("AHat %d:\n", i);
+            for (int j = 0; j < n1; j++) {
+                for (int k = 0; k < n2; k++) {
+                    printf("%f ", AHat[i * n1 * n2 + j * n2 + k]);
+                }
+                printf("\n");
+            }
+            printf("\n");
         }
     }
 
-    printf("print QR\n");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
-            printf("%f ", QR[i * n2 + j]);
-        }
-        printf("\n");
-    }
-
-    // free and destroy
+    // free arrays and destroy cHandle
     free(h_tau);
     gpuAssert(
         cudaFree(d_AHat));
