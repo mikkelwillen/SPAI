1d37a1d43f6435a04e35e3068241dfd0eabe13ed
carolineak <qlj556@alumni.ku.dk>
2023-05-04 16:07:48 +0200

    testing

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -110,7 +110,7 @@ int main() {
     
 
     
-    struct CSC* res = sequentialSpai(cscB, 0.01, 5, 1);
+    struct CSC* res = sequentialSpai(cscC, 0.01, 5, 1);
     printf("hallo?\n");
 
     // free memory
