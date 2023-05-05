bfa3fcbe93b95ac6d8f43790c98752d208272a9d
mikkelwillen <mikkel@willen.dk>
2023-05-05 11:20:17 +0200

    updateQR

--- CUDA/updateQR.cu.h
+++ CUDA/updateQR.cu.h
@@ -29,26 +29,26 @@ void* updateQR(CSC* A, float* AHat, float* Q, float* R, int* I, int* J, int* ITi
         }
     }
 
-    // set upper right square to AIJTilde of size n1 x n2Tilde
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2Tilde; j++) {
-            ATilde[i*n2Union + n2 + j] = AIJTilde[i*n2Tilde + j];
-        }
-    }
+    // // set upper right square to AIJTilde of size n1 x n2Tilde
+    // for (int i = 0; i < n1; i++) {
+    //     for (int j = 0; j < n2Tilde; j++) {
+    //         ATilde[i*n2Union + n2 + j] = AIJTilde[i*n2Tilde + j];
+    //     }
+    // }
 
-    // set lower left square to zeros of size n1Tilde x n2
-    for (int i = 0; i < n1Tilde; i++) {
-        for (int j = 0; j < n2; j++) {
-            ATilde[(n1 + i)*n2Union + j] = 0;
-        }
-    }
+    // // set lower left square to zeros of size n1Tilde x n2
+    // for (int i = 0; i < n1Tilde; i++) {
+    //     for (int j = 0; j < n2; j++) {
+    //         ATilde[(n1 + i)*n2Union + j] = 0;
+    //     }
+    // }
 
-    // set lower right square to AITildeJTilde of size n1Tilde x n2Tilde
-    for (int i = 0; i < n1Tilde; i++) {
-        for (int j = 0; j < n2Tilde; j++) {
-            ATilde[(n1 + i)*n2Union + n2 + j] = AITildeJTilde[i*n2Tilde + j];
-        }
-    }
+    // // set lower right square to AITildeJTilde of size n1Tilde x n2Tilde
+    // for (int i = 0; i < n1Tilde; i++) {
+    //     for (int j = 0; j < n2Tilde; j++) {
+    //         ATilde[(n1 + i)*n2Union + n2 + j] = AITildeJTilde[i*n2Tilde + j];
+    //     }
+    // }
 
     // print ATilde
     printf("ATilde:\n");
