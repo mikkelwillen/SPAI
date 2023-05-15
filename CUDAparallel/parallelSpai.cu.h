248c06cd99b5e1c4b44044f41e4993caf4d51501
mikkelwillen <mikkel@willen.dk>
2023-05-15 16:25:13 +0200

    csc memcpy

--- CUDAparallel/parallelSpai.cu.h
+++ CUDAparallel/parallelSpai.cu.h
@@ -103,7 +103,8 @@ __global__ void computeAHat(CSC* d_A, float** d_AHat, int** d_I, int** d_J, int*
         }
         __syncthreads();
         int offset = d_A->offset[J[j]];
-        if (i < maxn1 && j < maxn2 && l < offset) {
+        int offsetDiff = d_A->offset[J[j] + 1] - A->offset[J[j]];
+        if (i < maxn1 && j < maxn2 && l < offsetDiff) {
             if (I[i] == d_A->flatRowIndex[l + offset]) {
                 AHat[i * maxn2 + j] += d_A->flatData[l + offset];
             }
