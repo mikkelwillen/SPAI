edd0852b2ac2f965f5fabb4e8c75f75eb15939c6
mikkelwillen <mikkel@willen.dk>
2023-05-16 13:24:26 +0200

    qrBatched

--- CUDAparallel/parallelSpai.cu.h
+++ CUDAparallel/parallelSpai.cu.h
@@ -257,9 +257,9 @@ CSC* parallelSpai(CSC* A, float tolerance, int maxIterations, int s, const int b
         gpuAssert(
             cudaMalloc((void**) &d_Q, batchsize * maxn1 * maxn1 * sizeof(float)));
         gpuAssert(
-            cudaMalloc((void**) &d_Q, batchsize * sizeof(float*)));
+            cudaMalloc((void**) &d_PointerQ, batchsize * sizeof(float*)));
         numBlocks = (batchsize + BLOCKSIZE - 1) / BLOCKSIZE;
-        // deviceToDevicePointerKernel<<<numBlocks, BLOCKSIZE>>>(d_PointerQ, d_Q, batchsize, maxn1 * maxn1);
+        deviceToDevicePointerKernel<<<numBlocks, BLOCKSIZE>>>(d_PointerQ, d_Q, batchsize, maxn1 * maxn1);
 
         cudaDeviceSynchronize();
         // gpuAssert(
