14541e3dd56ae606ee204a970d797107ac546dcb
mikkelwillen <mikkel@willen.dk>
2023-05-22 16:03:31 +0200

    test 40x40

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -51,30 +51,30 @@ int main(int argc, char** argv) {
             I[i] = i;
             J[i] = i;
         }
-        float* CDense = CSCToDense(cscC, I, J, n, n);
-        printf("After CSCToDense\n");
-        float* resDense = CSCToDense(res, I, J, n, n);
-        printf("After CSCToDense\n");
+        // float* CDense = CSCToDense(cscC, I, J, n, n);
+        // printf("After CSCToDense\n");
+        // float* resDense = CSCToDense(res, I, J, n, n);
+        // printf("After CSCToDense\n");
         
-        // multiply CDense with resDense
-        printf("Mulitplying CDense with resDense\n");
-        float* identity = (float*) malloc(sizeof(float) * n * n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n;j++) {
-                identity[i * n + j] = 0.0;
-                for (int k = 0; k < n; k++) {
-                    identity[i * n + j] += CDense[i * n + k] * resDense[k * n + j];
-                }
-            }
-        }
+        // // multiply CDense with resDense
+        // printf("Mulitplying CDense with resDense\n");
+        // float* identity = (float*) malloc(sizeof(float) * n * n);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n;j++) {
+        //         identity[i * n + j] = 0.0;
+        //         for (int k = 0; k < n; k++) {
+        //             identity[i * n + j] += CDense[i * n + k] * resDense[k * n + j];
+        //         }
+        //     }
+        // }
     
-        printf("identity:\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n;j++) {
-                printf("%f ", identity[i * n + j]);
-            }
-            printf("\n");
-        }
+        // printf("identity:\n");
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n;j++) {
+        //         printf("%f ", identity[i * n + j]);
+        //     }
+        //     printf("\n");
+        // }
     
         // free memory
         freeCSC(cscA);
