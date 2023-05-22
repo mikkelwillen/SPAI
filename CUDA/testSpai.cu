98eaaa16d1f022ff56461f561dae50febc1a65a7
mikkelwillen <mikkel@willen.dk>
2023-05-22 16:04:17 +0200

    test 40x40

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -77,6 +77,7 @@ int main(int argc, char** argv) {
         // }
     
         // free memory
+        printf("freeing memory\n");
         freeCSC(cscA);
         freeCSC(cscB);
         freeCSC(cscC);
