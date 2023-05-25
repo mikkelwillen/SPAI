681a59263e0d168d6fb03d3e886b06efe8e3e689
mikkelwillen <mikkel@willen.dk>
2023-05-25 21:46:46 +0200

    test 4x4

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -66,11 +66,13 @@ int main(int argc, char** argv) {
             J[i] = i;
         }
 
-        float* CDense = CSCToDense(cscD, I, J, n, n);
+        float* CDense = CSCToDense(cscM4, I, J, n, n);
         printf("After CSCToDense\n");
         float* resDense = CSCToDense(res, I, J, n, n);
         printf("After CSCToDense\n");
         
+        
+
         // multiply CDense with resDense
         printf("Mulitplying CDense with resDense\n");
         float* identity = (float*) malloc(sizeof(float) * n * n);
