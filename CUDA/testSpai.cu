976037d1e256ca7ecb525b01adab784738d2a478
mikkelwillen <mikkel@willen.dk>
2023-05-26 15:47:40 +0200

    testing

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         initHwd();
         int m = 4;
-        int n = 5;
+        int n = 4;
         float sparsity = 0.5;
         float tolerance = 0.01;
         int maxIterations = 4;
