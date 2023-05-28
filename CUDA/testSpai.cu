bf67ded0f3d1d8de3638eccad9b85907c97b6fc6
mikkelwillen <mikkel@willen.dk>
2023-05-28 16:35:49 +0200

    perm test

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -82,8 +82,8 @@ int main(int argc, char** argv) {
         int n = 10;
         float sparsity = 0.5;
         float tolerance = 0.01;
-        int maxIterations = 9;
-        int s = 1;
+        int maxIterations = 4;
+        int s = 2;
     
         float* A = (float*) malloc(sizeof(float) * m * n);
         float* B = (float*) malloc(sizeof(float) * m * n);
