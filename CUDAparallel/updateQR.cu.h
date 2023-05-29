c95cba374351f21508faa73ace302dac46bdcf0e
mikkelwillen <mikkel@willen.dk>
2023-05-29 22:05:18 +0200

    parallel kernel

--- CUDAparallel/updateQR.cu.h
+++ CUDAparallel/updateQR.cu.h
@@ -150,6 +150,22 @@ int updateQR(cublasHandle_t cHandle, CSC* A, CSC* d_A, float** d_PointerQ, float
         cudaMalloc((void**) &d_PointerB2Q, batchsize * sizeof(float*)));
     gpuAssert(
         cudaMalloc((void**) &d_PointerB2R, batchsize * sizeof(float*)));
+
+    float* h_B2 = (float*) malloc(batchsize * maxn1Union * maxn2Tilde * sizeof(float));
+    gpuAssert(
+        cudaMemcpy(h_B2, d_B2, batchsize * maxn1Union * maxn2Tilde * sizeof(float), cudaMemcpyDeviceToHost));
+    printf("B2:\n");
+    for (int b = 0; b < batchsize; b++) {
+        printf("b = %d\n", b);
+        for (int i = 0; i < maxn1Union; i++) {
+            for (int j = 0; j < maxn2Tilde; j++) {
+                printf("%f ", h_B2[b * maxn1Union * maxn2Tilde + i * maxn2Tilde + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
     
     numBlocks = (batchsize + BLOCKSIZE - 1) / BLOCKSIZE;
     floatDeviceToDevicePointerKernel<<<numBlocks, BLOCKSIZE>>>(d_PointerB2Q, d_B2Q, batchsize, maxn1Union * maxn1Union);
