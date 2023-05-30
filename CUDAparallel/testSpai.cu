650cf367e5e86be39d8afa7f92853d1f280a9007
mikkelwillen <mikkel@willen.dk>
2023-05-30 17:26:33 +0200

    parallel stuff

--- CUDAparallel/testSpai.cu
+++ CUDAparallel/testSpai.cu
@@ -82,7 +82,7 @@ int main(int argc, char** argv) {
         int n = 10;
         float sparsity = 0.5;
         float tolerance = 0.01;
-        int maxIterations = 9;
+        int maxIterations = 3;
         int s = 1;
         int batchsize = 2;
     
