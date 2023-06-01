0f28370e29a75535cf42c0cc39c8d08a3b3403d9
mikkel <mikkel@willen.dk>
2023-06-01 22:27:34 +0200

    parallel

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -133,7 +133,7 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         initHwd();
         int m = 4;
-        int n = 20;
+        int n = 4;
         float sparsity = 1.0;
         float tolerance = 0.01;
         int maxIterations = 3;
