5d97ef50990cbf4e6091f4df73ef3035349809d5
carolineak <qlj556@alumni.ku.dk>
2023-06-01 14:00:14 +0200

    cusolver

--- CUDA/cuSOLVERInv.cu.h
+++ CUDA/cuSOLVERInv.cu.h
@@ -141,6 +141,33 @@ float* cuSOLVERInversion(float* A, int m, int n) {
     getLAndU<<<numBlocks, BLOCK_SIZE>>>(d_A, d_L, d_U, m, n);
     printf("L and U set\n");
 
+    float* h_L = (float*) malloc(m * n * sizeof(float));
+    float* h_U = (float*) malloc(m * n * sizeof(float));
+    gpuAssert(
+        cudaMemcpy(h_L, d_L, m * n * sizeof(float), cudaMemcpyDeviceToHost));
+    gpuAssert(
+        cudaMemcpy(h_U, d_U, m * n * sizeof(float), cudaMemcpyDeviceToHost));
+
+    // print h_L
+    printf("L:\n");
+    for (int i = 0; i < m; i++) {
+        printf("[");
+        for (int j = 0; j < n; j++) {
+            printf("%f ", h_L[i * n + j]);
+        }
+        printf("]\n");
+    }
+
+    // print h_U
+    printf("U:\n");
+    for (int i = 0; i < m; i++) {
+        printf("[");
+        for (int j = 0; j < n; j++) {
+            printf("%f ", h_U[i * n + j]);
+        }
+        printf("]\n");
+    }
+
 
     // Variables
     size_t workspaceInBytesOnDevice;
