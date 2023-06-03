89e24c11c69afeb95f39766e4e68f38eb44dec61
mikkelwillen <mikkel@willen.dk>
2023-06-03 19:31:50 +0200

    parallel kernels

--- CUDAparallel/invBatched.cu.h
+++ CUDAparallel/invBatched.cu.h
@@ -67,6 +67,17 @@ int invBatched(cublasHandle_t cHandle, float** d_PointerR, float** d_PointerInvR
         }
     }
 
+    int* h_PivotArray = (int*) malloc(maxn2 * batchsize * sizeof(int));
+    gpuAssert(
+        cudaMemcpy(h_PivotArray, d_PivotArray, maxn2 * batchsize * sizeof(int), cudaMemcpyDeviceToHost));
+    for (int i = 0; i < BATCHSIZE; i++) {
+        printf("Pivot array for matrix %d:\n", i);
+        for (int j = 0; j < maxn2; j++) {
+            printf("%d ", h_PivotArray[i * maxn2 + j]);
+        }
+        printf("\n");
+    }
+
     // run batched inversion from cublas
     // cublas docs: https://docs.nvidia.com/cuda/cublas/
     stat = cublasSgetriBatched(cHandle,
