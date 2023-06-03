b5fed3503f68dd85931bc76ae9bde4b2b97deaef
carolineak <qlj556@alumni.ku.dk>
2023-06-03 17:24:23 +0200

    C experiment

--- CUDA/sequentialTest.cu.h
+++ CUDA/sequentialTest.cu.h
@@ -48,7 +48,7 @@ int sequentialTestCuSOLVER(float* A, int n) {
         gettimeofday(&t_start, NULL);
 
         for(int i=0; i<RUNS_CPU; i++) {
-            float* res = cuSOLVERInversion(A, n, n);
+            cuSOLVERInversion(A, n, n);
         }
         
         cudaDeviceSynchronize();
@@ -56,7 +56,7 @@ int sequentialTestCuSOLVER(float* A, int n) {
         gettimeofday(&t_end, NULL);
         timeval_subtract(&t_diff, &t_end, &t_start);
         elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / RUNS_GPU;
-        gigaBytesPerSec = 2 * A->countNonZero * sizeof(int) * 1.0e-3f / elapsed;
+        gigaBytesPerSec = 2 * sizeof(int) * 1.0e-3f / elapsed;
         printf("\n\nSequential SPAI runs in: %lu microsecs, GB/sec: %.2f\n\n\n"
               , elapsed, gigaBytesPerSec);
     }
