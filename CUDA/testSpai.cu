e499ed1e7a3986d93bc92ecfc10a1b62299678ac
mikkelwillen <mikkel@willen.dk>
2023-06-04 12:49:05 +0200

    cusolver

--- CUDA/testSpai.cu
+++ CUDA/testSpai.cu
@@ -134,7 +134,7 @@ int main(int argc, char** argv) {
         initHwd();
         float tolerance = 0.01;
         int s = 1;
-        int n = 1000;
+        int n = 100;
         float sparsity = 0.1;
 
         float* test = (float*) malloc(sizeof(float) * n * n);
