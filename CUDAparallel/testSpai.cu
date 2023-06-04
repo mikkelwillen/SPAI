086f3d19e329adb819a6ccaa18c8616998b085ec
carolineak <qlj556@alumni.ku.dk>
2023-06-04 21:18:05 +0200

    kernel tests

--- CUDAparallel/testSpai.cu
+++ CUDAparallel/testSpai.cu
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         initHwd();
         int m = 4;
-        int n = 20;
+        int n = 10;
         float sparsity = 1.0;
         float tolerance = 0.01;
         int maxIterations = n - 1;
@@ -55,9 +55,11 @@ int main(int argc, char** argv) {
         struct CSC* cscD = createCSC(C, n, n);
         struct CSC* cscM4 = createCSC(m4, 4, 4);
 
-        runIdentityTest(cscC, n, n, sparsity, tolerance, maxIterations, s, batchsize);
+        // runIdentityTest(cscC, n, n, sparsity, tolerance, maxIterations, s, batchsize);
 
         // runMatrixMultiplicationTest();
+
+        parallelTest(cscA, tolerance, maxIterations, s, batchsize);
 
     } else if (argc == 8) {
         // read args
