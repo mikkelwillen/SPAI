cf0dbb373e103535acff8bf558abda4acc3a03bf
mikkelwillen <mikkel@willen.dk>
2023-06-04 16:54:30 +0200

    free all memory in parallel implementation

--- CUDAparallel/parallelSpai.cu.h
+++ CUDAparallel/parallelSpai.cu.h
@@ -708,10 +708,10 @@ CSC* parallelSpai(CSC* A, float tolerance, int maxIterations, int s, const int b
 
 
         // free memory
-        numBlocks = (batchsize + BLOCKSIZE - 1) / BLOCKSIZE;
-        freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerI, batchsize);
-        freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerJ, batchsize);
-        freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerSortedJ, batchsize);
+        // numBlocks = (batchsize + BLOCKSIZE - 1) / BLOCKSIZE;
+        // freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerI, batchsize);
+        // freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerJ, batchsize);
+        // freeArraysInPointerArray<<<numBlocks, BLOCKSIZE>>>(d_PointerSortedJ, batchsize);
 
         cudaDeviceSynchronize();
 
