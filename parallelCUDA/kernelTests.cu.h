9edd90669789d8f7616c83111743704a441f888c
mikkelwillen <mikkel@willen.dk>
2023-06-05 16:03:35 +0200

    kernel tests

--- parallelCUDA/kernelTests.cu.h
+++ parallelCUDA/kernelTests.cu.h
@@ -323,8 +323,8 @@ int runMatrixMultiplicationTest() {
 }
 
 int runSetSecondMatrixTest() {
-    int dim1 = 100;
-    int dim2 = 50;
+    int dim1 = 1000;
+    int dim2 = 500;
     float sparsity = 1.0;
     int batchsize = 1;
 
