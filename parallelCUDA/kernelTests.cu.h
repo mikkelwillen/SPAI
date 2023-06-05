360749153c1a574b859a7808e547c5431f2e0d04
mikkelwillen <mikkel@willen.dk>
2023-06-05 15:26:17 +0200

    kernel tests

--- parallelCUDA/kernelTests.cu.h
+++ parallelCUDA/kernelTests.cu.h
@@ -342,7 +342,7 @@ int runSetSecondMatrixTest() {
     cudaMalloc((void**)&A, batchsize * dim1 * dim1 * sizeof(float));
     cudaMalloc((void**)&B, batchsize * dim2 * dim2 * sizeof(float));
     
-    numBlocks = (batchsize * dim2 * dim2 + BLOCKSIZE - 1) / BLOCKSIZE;
+    int numBlocks = (batchsize * dim2 * dim2 + BLOCKSIZE - 1) / BLOCKSIZE;
     createRandomMatrix <<<numBlocks, BLOCKSIZE>>> (B, dim2, dim2, sparsity);
 
     cudaDeviceSynchronize();
